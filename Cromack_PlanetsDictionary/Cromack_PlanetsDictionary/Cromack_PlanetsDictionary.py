ceb47ad43d3c7cf1e9e52aa6249bd7f42d677a97
Rizez <>
2019-09-28 20:06:13 -0700

    Dictionary version of Planet project

--- Cromack_PlanetsDictionary/Cromack_PlanetsDictionary/Cromack_PlanetsDictionary.py
+++ Cromack_PlanetsDictionary/Cromack_PlanetsDictionary/Cromack_PlanetsDictionary.py
@@ -2,8 +2,8 @@
 # CromackT@student.elms.edu
 # 9/23/19
 # CIT-4205-DE1
-# Homework #1 Inter Planetary Weights
-# This program takes a users weight and multiplys theere weight by another planets gravity coefficent to return their weight on that planet.
+# Homework #1 Interplanetary Weights
+# This program takes a user's weight and multiplies their weight by another planets gravity coefficent to return their weight on that planet.
 
 #Create a Dictionary of Planet names and Gravity Coefficents
 planets = {"Mercury" : 0.38, "Venus" : 0.91, "our Moon" : 0.165, "Mars" : 0.38, "Juptier" : 2.34, "Saturn" : 0.93, "Uranus" : 0.92, "Neptune" : 1.12, "Pluto" : 0.066}
